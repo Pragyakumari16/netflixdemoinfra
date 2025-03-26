27617b4e03b0b708001fd2e7699de828fabe5b1f
Pragyakumari16 <praagyakumari@gmail.com>
2025-03-26 20:51:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-04aa00acb1165b32a"
+ami = "ami-08fe5144e4659a3b3 "
 instance_type = "t2.medium"
 key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+vpc_security_group_ids = ["sg-0b198925e0857f50f"]
 tags = {
 Name = var.instance_names[count.index]
 }
