4548f7c49fea39b3f96edb1d77a7bd9a2a42d002
Pragyakumari16 <praagyakumari@gmail.com>
2025-03-26 21:19:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-08fe5144e4659a3b3 "
-instance_type = "t2.medium"
-key_name = "argocd"
-vpc_security_group_ids = ["sg-0b198925e0857f50f"]
+ami = "ami-08fe5144e4659a3b3"
+instance_type = "t2.micro"
+key_name = "pragyajenkins"
+vpc_security_group_ids =["sg-0b198925e0857f50f"]
 tags = {
 Name = var.instance_names[count.index]
 }
